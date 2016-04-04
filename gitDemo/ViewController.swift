9a5148c8ac736dddacd94e6ddceb2a6eef69ac30
Vincent Chan <kinvincent@gmail.com>
2016-04-04 15:43:44 +0800

    Initial Commit

---
+++ gitDemo/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  gitDemo
+//
+//  Created by Vincent Chan on 4/4/16.
+//  Copyright © 2016 Vincent Chan. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
